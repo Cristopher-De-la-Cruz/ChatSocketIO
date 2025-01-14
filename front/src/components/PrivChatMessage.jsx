0e356209e21770fb19996d9af2b01758d849924a
Cristopher De la Cruz <cristopher.delacruz.1555@email.com>
2025-01-14 16:49:04 -0500

    chat funcional

--- front/src/components/PrivChatMessage.jsx
+++ front/src/components/PrivChatMessage.jsx
@@ -1,13 +1,16 @@
 import propTypes from 'prop-types';
+import { useState } from 'react';
 
-
-export const PrivChatMessage = ({message, type}) => {
+export const PrivChatMessage = ({ message, type }) => {
     console.log(type);
-    const messageType = type == 'enviado' ? 'end' : 'start';
+    const [messageType] = useState(type == 'enviado' ? 'end' : 'start');
     return (
-        <div className={`bg-white text-black p-2 rounded-lg max-w-[70%] self-${messageType}`}>
+        <div
+            className={`bg-white text-black p-2 rounded-lg max-w-[70%] self-${messageType} break-words whitespace-pre-wrap`}
+        >
             {message}
         </div>
+
     )
 }
 PrivChatMessage.propTypes = {
